fcc23f1afbfc167518ebf1862809f9bc15eed966
ItsWitherMC <54366349+ItsWitherMC@users.noreply.github.com>
2022-12-04 20:00:11 +0700

    Fix Console Fakeop Bug

--- global/src/main/java/withicality/csmp/global/commands/FakeopCommand.java
+++ global/src/main/java/withicality/csmp/global/commands/FakeopCommand.java
@@ -3,6 +3,7 @@ package withicality.csmp.global.commands;
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolManager;
 import com.comphenix.protocol.events.PacketContainer;
+import com.google.common.collect.ImmutableList;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
@@ -16,7 +17,7 @@ import java.util.List;
 
 public class FakeopCommand extends CosmicCommand {
     public FakeopCommand() {
-        super("fakeop", "", "", Collections.singletonList("demotroll"), "csmp.demotroll");
+        super("fakeop", "To fake op a player", "/fakeop <player>", ImmutableList.of(), "csmp.demotroll");
     }
 
     @Override
@@ -27,11 +28,11 @@ public class FakeopCommand extends CosmicCommand {
         }
         Player victim = Bukkit.getPlayerExact(args[0]);
         if (victim == null) {
-            noPlayerFound(args[0], player);
+            noPlayerFound(args[0], sender);
             return;
         }
 
-        victim.sendMessage(ChatColor.GRAY + "" + ChatColor.ITALIC + "["+ player.getName() + ": Made " + victim.getName() + " a server operator]");
+        victim.sendMessage(ChatColor.GRAY + "" + ChatColor.ITALIC + "["+ sender.getName() + ": Made " + victim.getName() + " a server operator]");
         sender.sendMessage(ChatColor.AQUA + "Fake-opped " + ChatColor.DARK_AQUA + victim.getName() + ChatColor.AQUA + ".");
     }
 }
