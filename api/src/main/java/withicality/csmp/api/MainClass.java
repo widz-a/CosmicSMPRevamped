9a50386b6fc5a911724a5fc528a8bbdc17e3cc10
ItsWitherMC <54366349+ItsWitherMC@users.noreply.github.com>
2022-12-05 20:57:56 +0700

    ??? player count compatible with supervanish

--- api/src/main/java/withicality/csmp/api/MainClass.java
+++ api/src/main/java/withicality/csmp/api/MainClass.java
@@ -2,6 +2,7 @@ package withicality.csmp.api;
 
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.PacketListener;
 import org.bukkit.plugin.java.JavaPlugin;
 import withicality.csmp.api.protocol.PlayerCountListener;
 
@@ -10,6 +11,12 @@ public class MainClass extends JavaPlugin {
     public void onEnable() {
         ProtocolManager manager = ProtocolLibrary.getProtocolManager();
         APIStuff.setManager(manager);
+
+        for (PacketListener listener : manager.getPacketListeners()) {
+            if (!listener.getPlugin().getName().equals("SuperVanish")) continue;
+            if (!listener.getClass().getSimpleName().equals("ServerListPacketListener")) continue;
+            manager.removePacketListener(listener);
+        }
 
         manager.addPacketListener(new PlayerCountListener());
 
