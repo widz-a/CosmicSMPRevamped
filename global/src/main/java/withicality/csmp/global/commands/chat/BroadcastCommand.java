858f92a019668135832286a365c494b14f4b1574
ItsWitherMC <54366349+ItsWitherMC@users.noreply.github.com>
2022-12-25 20:52:05 +0700

    <proximity_alt> make it white

--- global/src/main/java/withicality/csmp/global/commands/chat/BroadcastCommand.java
+++ global/src/main/java/withicality/csmp/global/commands/chat/BroadcastCommand.java
@@ -6,12 +6,12 @@ import withicality.csmp.api.CosmicCommand;
 
 public class BroadcastCommand extends CosmicCommand {
     public BroadcastCommand() {
-        super("broadcast|bc", "Broadcast to the server. The default color is " + ChatColor.GOLD + "GOLD", "/broadcast <message>", "csmp.broadcast");
+        super("broadcast|bc", "Broadcast to the server. The default color is " + ChatColor.WHITE + "WHITE", "/broadcast <message>", "csmp.broadcast");
         setMinArguments(1);
     }
 
     @Override
     protected void onCommand() {
-        Bukkit.broadcastMessage(ChatColor.GOLD + ChatColor.translateAlternateColorCodes('&', String.join(" ", args)));
+        Bukkit.broadcastMessage(ChatColor.WHITE + ChatColor.translateAlternateColorCodes('&', String.join(" ", args)));
     }
 }
